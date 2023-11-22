5526844cfd8665009de400a27664f8a6d89e205b
stapia casa <santiago.tapia@upm.es>
2023-11-22 13:49:33 +0100

    add hints to platform virtual methods

--- src/as2_platform_dji_psdk.cpp
+++ src/as2_platform_dji_psdk.cpp
@@ -1,14 +1,19 @@
 #include "as2_platform_dji_psdk.hpp"
+#include "details/as2_dji_matrice_psdk_platform_impl.hpp"
 
 namespace as2_platform_dji_psdk
 {
 
 DJIMatricePSDKPlatform::DJIMatricePSDKPlatform(const rclcpp::NodeOptions & options)
-: as2::AerialPlatform(options) {}
+: as2::AerialPlatform(), _impl(new DJIMatricePSDKPlatform_impl{}) {}
 
 void DJIMatricePSDKPlatform::configureSensors()
 {
-  // Configure sensors here
+  _impl->init(this);
+  if (!_impl->setLocalPositionService.wait_for_service()) {
+    // TODO: Since waiting is cancelled, is it neccesary any further action?
+    RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+  }
 }
 
 bool DJIMatricePSDKPlatform::ownSetArmingState(bool state)
@@ -31,8 +36,70 @@ bool DJIMatricePSDKPlatform::ownSetPlatformControlMode(const as2_msgs::msg::Cont
 
 bool DJIMatricePSDKPlatform::ownSendCommand()
 {
-  // Send command to platform here
-  return false;
+  // TODO: Look for the service to set reference and get control authority
+  if (platform_info_msg_.current_control_mode.control_mode == as2_msgs::msg::ControlMode::HOVER) {
+    // send all zeros
+    _impl->velocityCommand->axes[0] = 0.0f;  // x(m)
+    _impl->velocityCommand->axes[1] = 0.0f;
+    _impl->velocityCommand->axes[2] = 0.0f;
+    _impl->velocityCommand->axes[3] = 0.0f;  // yaw (rad)
+    _impl->velocityCommand.publish();
+    RCLCPP_INFO(this->get_logger(), " HOVERING");
+    return true;
+  }
+  /*
+  if (!this->has_new_references_) {
+    RCLCPP_DEBUG_THROTTLE(this->get_logger(), *this->get_clock(), 1000,
+                          "No new references since mode change");
+    return true;
+  }
+  double x, y, z, yaw;
+  x = y = z = yaw = 0.0;
+
+  if (platform_info_msg_.current_control_mode.yaw_mode == as2_msgs::msg::ControlMode::YAW_ANGLE) {
+    tf2::Quaternion q(
+        this->command_pose_msg_.pose.orientation.x, this->command_pose_msg_.pose.orientation.y,
+        this->command_pose_msg_.pose.orientation.z, this->command_pose_msg_.pose.orientation.w);
+    tf2::Matrix3x3 m(q);
+    double _a, _b, yaw_;
+    m.getRPY(_a, _b, yaw);
+    yaw = -yaw * 180.0 / M_PI;
+  } else if (platform_info_msg_.current_control_mode.yaw_mode ==
+             as2_msgs::msg::ControlMode::YAW_SPEED) {
+    yaw = -this->command_twist_msg_.twist.angular.z * 180.0 / M_PI;
+  } else {
+    RCLCPP_ERROR(this->get_logger(), "Unknown yaw mode");
+    return false;
+  }
+
+  switch (platform_info_msg_.current_control_mode.control_mode) {
+    // case as2_msgs::msg::ControlMode::POSITION: {
+    // } break;
+    case as2_msgs::msg::ControlMode::SPEED: {
+      // Conversion from AS2 ENU frame into DJI NEU frame
+      x = this->command_twist_msg_.twist.linear.y;
+      y = this->command_twist_msg_.twist.linear.x;
+      z = this->command_twist_msg_.twist.linear.z;
+    } break;
+    // case as2_msgs::msg::ControlMode::SPEED_IN_A_PLANE: {
+    // } break;
+    // case as2_msgs::msg::ControlMode::ACRO: {
+    // } break;
+    default:
+      RCLCPP_ERROR(this->get_logger(), "Unknown control mode in send command");
+      return false;
+  }
+  // vehicle_->flightController->setJoystickMode(dji_joystick_mode_);
+  // FlightController::JoystickCommand joystick_cmd = {
+  //     (float)x,
+  //     (float)y,
+  //     (float)z,
+  //     (float)yaw,
+  // };
+  // vehicle_->flightController->setJoystickCommand(joystick_cmd);
+  // vehicle_->flightController->joystickAction();
+  */
+  return true;
 }
 
 void DJIMatricePSDKPlatform::ownStopPlatform()
