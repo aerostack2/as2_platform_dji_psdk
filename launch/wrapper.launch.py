b5969a65c1946a4c64c161db5b977d2d19cd65aa
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2024-01-22 18:12:43 +0100

    Improve wrapper launcher

--- launch/wrapper.launch.py
+++ launch/wrapper.launch.py
@@ -7,14 +7,13 @@
 # This is a ROS2 launch file which starts the psdk_wrapper_node,
 # configures it and activates it.
 
-import os
-
 from launch import LaunchDescription
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import LaunchConfiguration
 from launch.actions import EmitEvent, DeclareLaunchArgument
 from launch_ros.actions import LifecycleNode
 from launch_ros.events.lifecycle import ChangeState
+from launch.substitutions import LaunchConfiguration, EnvironmentVariable, PathJoinSubstitution
 
 import lifecycle_msgs.msg
 import launch
@@ -22,31 +21,22 @@ import launch
 
 def generate_launch_description():
     # Declare the namespace launch argument
-    declare_namespace_cmd = DeclareLaunchArgument(
-        "namespace",
-        default_value="wrapper",
-        description="Namespace of the node",
-    )
-
-    namespace = LaunchConfiguration("namespace")
-    # Get wrapper parameters
-    psdk_wrapper_pkg_share = FindPackageShare(
-        "psdk_wrapper").find("as2_platform_dji_psdk")
-    wrapper_params = os.path.join(
-        psdk_wrapper_pkg_share,
-        "config",
-        "psdk_params.yaml",
-    )
+    psdk_params_file = PathJoinSubstitution([
+        FindPackageShare('as2_platform_dji_psdk'),
+        'config', 'psdk_params.yaml'
+    ])
 
     # Prepare the wrapper node
     wrapper_node = LifecycleNode(
         package="psdk_wrapper",
         executable="psdk_wrapper_node",
         name="psdk_wrapper_node",
-        output='screen',
+        namespace=LaunchConfiguration('namespace'),
+        output="screen",
         emulate_tty=True,
-        namespace=namespace,
-        parameters=[wrapper_params],
+        parameters=[
+                LaunchConfiguration('psdk_params'),
+        ],
         remappings=[
             ("psdk_ros2/gps_position_fused", "sensor_measurements/gps"),
             ("psdk_ros2/imu", "sensor_measurements/imu")
@@ -70,10 +60,17 @@ def generate_launch_description():
     )
 
     # Create LaunchDescription and populate
-    ld = LaunchDescription()
+    ld = LaunchDescription([
+        DeclareLaunchArgument('namespace',
+                              default_value=EnvironmentVariable(
+                                  'AEROSTACK2_SIMULATION_DRONE_ID'),
+                              description='Drone namespace'),
+        DeclareLaunchArgument('psdk_params',
+                              default_value=psdk_params_file,
+                              description='DJI PSDK configuration file')
+    ])
 
     # Declare Launch options
-    ld.add_action(declare_namespace_cmd)
     ld.add_action(wrapper_node)
     ld.add_action(wrapper_configure_trans_event)
     ld.add_action(wrapper_activate_trans_event)
